76b70e865e0a360060b927242adb1c5dacd04082
Kevin <kevin.ortega@student.umn.ac.id>
2019-12-13 12:55:24 +0700

    third commit

---
+++ src/app/my-post/edit-mypost/edit-mypost.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { EditMypostPage } from './edit-mypost.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EditMypostPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [EditMypostPage]
+})
+export class EditMypostPageModule {}
