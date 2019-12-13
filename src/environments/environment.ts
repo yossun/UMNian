f06e8a756fa9f22270391721bed05e2433950788
Kevin <kevin.ortega@student.umn.ac.id>
2019-12-13 12:53:01 +0700

    third commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyDVFv4DlF9_Q7_x8TC5hHjqraiflN7EXys'
 };
 
 /*
